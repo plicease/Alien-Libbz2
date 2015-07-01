ab89ed0488f1b79ddfdde3111ce5dc74d35aecb6
Graham Ollis <perl@wdlabs.com>
2015-07-01 17:02:01 -0400

    doco

--- README.md
+++ README.md
@@ -2,6 +2,32 @@
 
 Build and make available bz2
 
+# SYNOPSIS
+
+    use Alien::Libbz2;
+    
+    my $cflags = Alien::Libbz2->cflags;
+    my $libs   = ALien::Libbz2->libs;
+
+# DESCRIPTION
+
+This [Alien](https://metacpan.org/pod/Alien) module provides the necessary compiler and linker flags needed
+for using libbz2 in XS.
+
+# METHODS
+
+## cflags
+
+    my $cflags = Alien::Libbz2->cflags;
+
+Returns the C compiler flags.
+
+## libs
+
+    my $libs = Alien::Libbz2->libs;
+
+Returns the linker flags.
+
 # AUTHOR
 
 Graham Ollis <plicease@cpan.org>
--- lib/Alien/Libbz2.pm
+++ lib/Alien/Libbz2.pm
@@ -8,6 +8,34 @@ use parent 'Alien::Base';
 # ABSTRACT: Build and make available bz2
 # VERSION
 
+=head1 SYNOPSIS
+
+ use Alien::Libbz2;
+ 
+ my $cflags = Alien::Libbz2->cflags;
+ my $libs   = ALien::Libbz2->libs;
+
+=head1 DESCRIPTION
+
+This L<Alien> module provides the necessary compiler and linker flags needed
+for using libbz2 in XS.
+
+=head1 METHODS
+
+=head2 cflags
+
+ my $cflags = Alien::Libbz2->cflags;
+
+Returns the C compiler flags.
+
+=head2 libs
+
+ my $libs = Alien::Libbz2->libs;
+
+Returns the linker flags.
+
+=cut
+
 sub libs {
   my($class) = @_;
   $class->install_type eq 'system' ? '-lbz2' : $class->SUPER::libs;
